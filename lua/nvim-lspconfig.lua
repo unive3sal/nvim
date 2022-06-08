f17b08cd4c00cee1013386aa47b50901a1ad73d0
metaCoder-00 <dengyuhao_123@163.com>
2022-06-08 15:32:27 +0800

    modify lsp startup

--- lua/nvim-lspconfig.lua
+++ lua/nvim-lspconfig.lua
@@ -29,6 +29,8 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { 'clangd' }
@@ -38,7 +40,8 @@ for _, lsp in pairs(servers) do
     flags = {
       -- This will be the default in neovim 0.7+
       debounce_text_changes = 150,
-    }
+    },
+    capabilities = capabilities
   }
 end
 
