c5327745f18006595e0a17d19f2763513c65b843
metaCoder-00 <dengyuhao_123@163.com>
2022-07-25 15:36:52 +0800

    add lua lsp

--- lua/nvim-lspconfig.lua
+++ lua/nvim-lspconfig.lua
@@ -44,7 +44,11 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 --     capabilities = capabilities
 --   }
 -- end
-require('lspconfig')['clangd'].setup {
+
+local lspconfig = require('lspconfig')
+
+-- clangd
+lspconfig.clangd.setup {
   on_attach = on_attach,
   flags = {
     -- This will be the default in neovim 0.7+
@@ -64,5 +68,29 @@ require('lspconfig')['clangd'].setup {
     '--log=verbose'
   },
   filetypes = {"c", "cpp", "objc", "objcpp"}
+}
+
+-- lua
+lspconfig.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
 
