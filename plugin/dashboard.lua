1d0e134c7a039d1b6721e9e4b4b11de48b52209a
metaCoder-00 <dengyuhao.233@outlook.com>
2024-01-14 10:16:55 +0800

    config dashboard

--- plugin/dashboard.lua
+++ plugin/dashboard.lua
@@ -1,20 +1,36 @@
 require'dashboard'.setup {
+  theme = 'hyper',
   config = {
-    header = {
-      '     *          *                                                                           ',
-      '   *****        ***                                                                         ',
-      ' ********       *****                                                  @@@                  ',
-      '**********      ******                                                                      ',
-      '****#******     ######      @ @@@@@@     @@@@@     @@@@@@  @@@     @@@ @@@  @@@@@@@@ @@@@@@ ',
-      '*****#*******   ######      @@     @@  @       @ @@      @@ @@@   @@@  @@@  @@@   @@@    @@@',
-      '****** *******  ######      @       @ @@@@@@@@@@ @        @  @@@  @@   @@@  @@@   @@@    @@@',
-      '######  ******* ######      @       @ @@         @        @  @@@ @@@   @@@  @@@   @@@    @@@',
-      '######   #############      @       @  @         @@      @@   @@@@@    @@@  @@@   @@@    @@@',
-      '######     ###########      @       @   @@   @@   @@    @@     @@@     @@@  @@@   @@@    @@@',
-      '######      ##########                                                                      ',
-      ' #####       ########                                                                       ',
-      '   ###        #####                                                                         ',
-      '     #          #                                                                           ',
-    }
+    week_header = {
+     enable = true,
+    },
+    shortcut = {
+      {
+        desc = '󰊳 Update',
+        group = '@property',
+        action = 'Pckr update',
+        key = 'u'
+      },
+      {
+        icon = ' ',
+        icon_hl = '@variable',
+        desc = 'Files',
+        group = 'Label',
+        action = 'FzfLua files',
+        key = 'f',
+      },
+      {
+        desc = ' Grep',
+        group = 'DiagnosticHint',
+        action = 'FzfLua live_grep_glob',
+        key = 'g',
+      },
+      --{
+      --  desc = ' Explore',
+      --  group = 'Number',
+      --  action = 'NvimTreeToggle',
+      --  key = 'e',
+      --},
+    },
   },
 }
